22def00a4a013ec7a1fc86c259cb7fa0b7a38eea
Bruno Ferreira <shirayukikitsune@gmail.com>
2023-03-28 20:33:39 -0300

    Inicializa repositório

---
+++ src/main/java/br/com/faculdadeimpacta/aluno/charlie/agendaplus/AgendaplusApplication.java
@@ -0,0 +1,13 @@
+package br.com.faculdadeimpacta.aluno.charlie.agendaplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/faculdadeimpacta/aluno/charlie/agendaplus/AgendaplusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.faculdadeimpacta.aluno.charlie.agendaplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
