c0b810380ca5838ddffa9fcd56124a76f7a00807
Bruno Ferreira <bruno.rferreira@aluno.faculdadeimpacta.com.br>
2023-04-11 20:03:07 -0300

    Corrige mapeamento da coluna

--- src/main/java/br/com/faculdadeimpacta/aluno/charlie/agendaplus/entity/ContactData.java
+++ src/main/java/br/com/faculdadeimpacta/aluno/charlie/agendaplus/entity/ContactData.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.UUID;
 
@@ -17,6 +14,7 @@ import java.util.UUID;
 @Table(name = "contact_data")
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString(exclude = "contact")
 public class ContactData {
     @Id
     @Column(name = "contact_data_uuid")
@@ -32,7 +30,7 @@ public class ContactData {
     @NotBlank
     private String value;
     @JsonIgnore
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "contact_uuid", referencedColumnName = "contact_uuid", nullable = false, insertable = false, updatable = false)
     private Contact contact;
 }
